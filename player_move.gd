dc3c7f81afb4891d24297ace320010b4a0be186c
AtlasleDev <jacerstopel@gmail.com>
2025-10-18 22:13:27 -0500

    PUSHH

--- player_move.gd
+++ player_move.gd
@@ -2,19 +2,27 @@ extends Camera2D
 
 func _process(delta):
 	var direction = Vector2.ZERO
+	
+	var moving = false
 
 	if Input.is_action_pressed("ui_up"):
+		moving = true
 		direction.y -= 1
-		$Player.play("default")
 	if Input.is_action_pressed("ui_down"):
+		moving = true
 		direction.y += 1
-		$Player.play("default")
 	if Input.is_action_pressed("ui_left"):
+		moving = true
 		direction.x -= 1
-		$Player.play("default")
 	if Input.is_action_pressed("ui_right"):
+		moving = true
 		direction.x += 1
+		
+	if moving:
 		$Player.play("default")
+	elif !moving:
+		$Player.stop()
+		$Player.play("Idle")
 
 		# Normalize the direction vector to prevent faster diagonal movement
 	if direction.length() > 0:
